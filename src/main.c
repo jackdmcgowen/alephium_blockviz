b796b0f0b4394a4ecd9fd169bbc2984204e40eca
Jack McGowen <jackdmcgowen@gmail.com>
2025-02-20 21:01:31 -0600

    add initial logic

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <curl/curl.h>
-#include <cJSON.h>
+#include <cjson/cJSON.h>
 #include <stdio.h>
 
 size_t write_callback
@@ -10,23 +10,26 @@ size_t write_callback
 	void 				*userp
 	) 
 {
+char                   *payload;
 cJSON                  *json;
-cJSON                  *shards;
+//cJSON                  *shards;
 cJSON                  *height;
+
+printf( "%.*s\n", (int)(size * nmemb), (char*)contents );
 
 json = cJSON_ParseWithLength( contents, size * nmemb );
 if( !json ) 
     {
     printf( "JSON parse failed\n" );
-    return size * nmemb;
+    return( size * nmemb );
     }
     
-shards = cJSON_GetObjectItem( json, "currentShards" );
+//shards = cJSON_GetObjectItem( json, "currentShards" );
 height = cJSON_GetObjectItem( json, "currentHeight" );
 
-if( shards && height )
+if( /* shards  && */ height)
     {
-    printf( "Shards: %d, Chain Height: %d\n", shards->valueint, height->valueint );
+    printf( /*"Shards: %d, */"Chain Height : % d\n", /* shards->valueint, */ height->valueint);
     }
 else
     {
@@ -46,13 +49,16 @@ int main
 {
 CURL                   *curl;
 
+const char* testnet_url = "https://node.testnet.alephium.org/blockflow/chain-info/?fromGroup=0&toGroup=3";
+
 curl = curl_easy_init();
 
 if( curl )
     {
     CURLcode        res;
+    uint32_t        httpCode;
     
-    curl_easy_setopt( curl, CURLOPT_URL, "http://testnet.alephium.org/blockflow/chain-info" );
+    curl_easy_setopt( curl, CURLOPT_URL, testnet_url );
     curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, write_callback );
     
     res = curl_easy_perform( curl );
@@ -61,6 +67,25 @@ if( curl )
         {
         fprintf( stderr, "curl failed: %s\n", curl_easy_strerror( res ) );
         }
+    else
+    {
+    curl_easy_getinfo( curl, CURLINFO_RESPONSE_CODE, &httpCode );
+    switch( httpCode )
+        {
+        case 404:
+            printf( "HTTP 404 - Not Found\n" );
+            break;
+
+        case 200:
+            printf( "HTTP 200 - OK\n" );
+            break;
+
+        default:
+            printf( "HTTP error: %ld\n", httpCode );
+            break;
+
+        }
+    }
         
     curl_easy_cleanup( curl );
     }
